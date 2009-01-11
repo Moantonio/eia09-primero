7f0c75bdebdc8e1b75e813c2d367f53c7e0eb38b
donvicentedelamancha <donvicentedelamancha@5aebf2f8-9784-11dd-b31e-5b0f3c431db7>
2009-01-11 17:26:30 +0000

    Clase que carga un proyecto.

--- EIA09Struts/WEB-INF/src/eia/load/LoadProject.java
+++ EIA09Struts/WEB-INF/src/eia/load/LoadProject.java
@@ -1,24 +1,52 @@
 package eia.load;
 
+import java.io.File;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LoadProject extends ActionSupport{
-	
-	String projectName;
+import eia.model.Proyecto;
+import eia.util.xml.XMLProyecto;
 
-	public String execute() throws Exception {
-	    System.out.println("Loading project...");
-	    String fileName = getProjectName();
-	    //Aqui se procesaria el fichero para cargar la informacion
-	    //contenida en el .xml en objetos JAVA
-	    return SUCCESS;
-	  }
-	
-	public String getProjectName() {
-		return projectName;
+
+public class LoadProject extends ActionSupport {
+  
+	private File upload;//The actual file
+	private String uploadContentType; //The content type of the file
+	private String uploadFileName; //The uploaded file name
+    
+    public String execute() throws Exception {
+    	try {
+    		XMLProyecto xmlProy = new XMLProyecto("plantillas\\proyectoEIA.dtd");
+    		Proyecto proyecto = (Proyecto)xmlProy.leer(upload.getAbsolutePath());
+    		return SUCCESS;
+    	} 
+    	catch (Exception e){
+    		addActionError(e.getMessage());
+    		return ERROR;
+    	}
+
+    }
+	  
+    public File getUpload() {
+	    return upload;
 	}
-
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}	
+	  
+    public void setUpload(File upload) {
+	    this.upload = upload;
+	}
+	
+    public String getUploadContentType() {
+	    return uploadContentType;
+	}
+    
+	public void setUploadContentType(String uploadContentType) {
+	    this.uploadContentType = uploadContentType;
+	}
+	  
+	public String getUploadFileName() {
+	    return uploadFileName;
+	}
+	  
+	public void setUploadFileName(String uploadFileName) {
+	    this.uploadFileName = uploadFileName;
+	}  
 }
